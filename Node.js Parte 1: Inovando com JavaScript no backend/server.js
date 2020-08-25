0ac2ab518468759266e312c45509d6027bcbcfa3
Marcelo Norberto <norbertomarcelo8@gmail.com>
2020-08-25 20:34:14 -0300

    Subindo servidor da maneira mais simples possivel

---
+++ Node.js Parte 1: Inovando com JavaScript no backend/server.js
@@ -0,0 +1,32 @@
+const http = require("http");
+
+const servidor = http.createServer(function (req, res) {
+  let html = "";
+
+  if (req.url == "/") {
+    html = `
+    <html>
+      <head>
+        <meta charset="utf-8"/>
+      </head>
+      <body>
+        <h1>Olá Node!</h1>
+      </body>
+    </html>
+  `;
+  } else if (req.url == "/js") {
+    html = `
+    <html>
+      <head>
+        <meta charset="utf-8"/>
+      </head>
+      <body>
+        <h1>Olá Javascript</h1>
+      </body>
+    </html>
+    `;
+  }
+
+  res.end(html);
+});
+servidor.listen(3000);
