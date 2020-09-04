29d42581248f1e76f4ef1866888adce423694be8
Marcelo Norberto <norbertomarcelo8@gmail.com>
2020-09-03 21:29:47 -0300

    Middlewares

--- Node.js-Parte-1-Inovando-com-JavaScript-no-backend/src/app/infra/livro-dao.js
+++ Node.js-Parte-1-Inovando-com-JavaScript-no-backend/src/app/infra/livro-dao.js
@@ -3,6 +3,27 @@ class LivroDao {
     this._db = db;
   }
 
+  adiciona(livro) {
+    return new Promise((resolve, reject) => {
+      this._db.run(
+        `
+          INSERT INTO LIVROS (
+            titulo,
+            preco,
+            descricao
+          ) values (?, ?, ?)
+        `,
+        [livro.titulo, livro.preco, livro.descricao],
+        function (erro) {
+          if (erro) {
+            console.log(erro);
+            return reject("Não foi possível listar os livros");
+          }
+        }
+      );
+    });
+  }
+
   lista() {
     return new Promise((resolve, reject) => {
       this._db.all("SELECT * FROM livros", (erro, resultados) => {
--- Node.js-Parte-1-Inovando-com-JavaScript-no-backend/src/app/rotas/rotas.js
+++ Node.js-Parte-1-Inovando-com-JavaScript-no-backend/src/app/rotas/rotas.js
@@ -28,4 +28,18 @@ module.exports = (app) => {
       )
       .catch((erro) => console.log(erro));
   });
+
+  app.get("/livros/form", function (req, res) {
+    res.marko(require("../views/livros/form/form.marko"));
+  });
+
+  app.post("/livros", function (req, res) {
+    console.log(req.body);
+
+    const livroDao = new LivroDao(db);
+    livroDao
+      .adiciona(req.body)
+      .then(res.redirect("/livros"))
+      .catch((erro) => console.log(erro));
+  });
 };
