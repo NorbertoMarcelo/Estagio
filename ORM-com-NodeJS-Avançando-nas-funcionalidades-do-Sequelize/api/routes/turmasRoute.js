7024dffe60d2005f02c567b440f05ebc5fec291e
Marcelo Norberto <norbertomarcelo8@gmail.com>
2020-10-28 20:59:37 -0300

    Usando soft delete

---
+++ ORM-com-NodeJS-Avançando-nas-funcionalidades-do-Sequelize/api/routes/turmasRoute.js
@@ -0,0 +1,13 @@
+const { Router } = require("express");
+const TurmasController = require("../controllers/TurmasController");
+
+const router = Router();
+
+router
+  .get("/turmas", TurmasController.pegaTodasAsTurmas)
+  .get("/turmas/:id", TurmasController.pegaUmaTurma)
+  .post("/turmas", TurmasController.criaTurma)
+  .put("/turmas/:id", TurmasController.atualizaTurma)
+  .delete("/turmas/:id", TurmasController.apagaTurma);
+
+module.exports = router;
