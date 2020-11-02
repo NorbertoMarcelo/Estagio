40973a793006624d8f928d3c5603e7ad66e8a17a
Marcelo Norberto <norbertomarcelo8@gmail.com>
2020-11-02 16:59:31 -0300

    Usando Transacoes

--- ORM-com-NodeJS-Avançando-nas-funcionalidades-do-Sequelize/api/controllers/PessoaController.js
+++ ORM-com-NodeJS-Avançando-nas-funcionalidades-do-Sequelize/api/controllers/PessoaController.js
@@ -194,6 +194,29 @@ class PessoaController {
       return res.status(500).json(error.message);
     }
   }
+
+  static async cancelaPessoa(req, res) {
+    const { estudanteId } = req.params;
+    try {
+      database.sequelize.transaction(async (transacao) => {
+        await database.Pessoas.update(
+          { ativo: false },
+          { where: { id: Number(estudanteId) } },
+          { transaction: transacao }
+        );
+        await database.Matriculas.update(
+          { status: "cancelado" },
+          { where: { estudante_id: Number(x) } },
+          { transaction: transacao }
+        );
+        return res.status(200).json({
+          message: `Matricula ref. estudante ${estudanteId} cancelada.`,
+        });
+      });
+    } catch (error) {
+      return res.status(500).json(error.message);
+    }
+  }
 }
 
 module.exports = PessoaController;
--- ORM-com-NodeJS-Avançando-nas-funcionalidades-do-Sequelize/api/routes/pessoasRoute.js
+++ ORM-com-NodeJS-Avançando-nas-funcionalidades-do-Sequelize/api/routes/pessoasRoute.js
@@ -2,7 +2,7 @@ const { Router } = require("express");
 const PessoaController = require("../controllers/PessoaController");
 
 const router = Router();
-
+// GET
 router
   .get("/pessoas", PessoaController.pegaPessoasAtivas)
   .get("/pessoas/todos", PessoaController.pegaTodasAsPessoas)
@@ -17,19 +17,20 @@ router
     PessoaController.pegaMatriculasPorTurma
   )
   .get("/pessoas/matricula/lotada", PessoaController.pegaTurmasLotadas);
-
+// POST
 router
   .post("/pessoas", PessoaController.criaPessoa)
   .post("/pessoas/:estudanteId/matricula", PessoaController.criaMatricula)
-  .post("/pessoas/:id/restaura", PessoaController.restauraPessoa);
-
+  .post("/pessoas/:id/restaura", PessoaController.restauraPessoa)
+  .post("/pessoas/:estudanteId/cancela", PessoaController.cancelaPessoa);
+// PUT
 router
   .put("/pessoas/:id", PessoaController.atualizaPessoa)
   .put(
     "/pessoas/:estudanteId/matricula/:matriculaId",
     PessoaController.atualizaMatricula
   );
-
+// DELETE
 router
   .delete("/pessoas/:id", PessoaController.apagaPessoa)
   .delete(
